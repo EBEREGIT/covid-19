3a4c78e05d30e3de536670a84d26e74f0f3a9bc1
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-06 22:07:48 +0100

    finish nigeria styling

--- src/components/Nigeria.js
+++ src/components/Nigeria.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import {
   Container,
   Row,
-  Col,
-  Card,
-  ListGroup,
+  Col
 } from "react-bootstrap/";
 import NumberFormat from "react-number-format";
 import Loading from "./Loading";
@@ -19,6 +17,7 @@ export default class Nigeria extends Component {
     };
   }
 
+  // fetch states from API
   componentDidMount() {
     fetch("https://covidnigeria.herokuapp.com/api")
       .then((res) => res.json())
@@ -41,6 +40,7 @@ export default class Nigeria extends Component {
     let stateReport = {};
     let totalNigeriaReport = {};
     let totalActive = 0;
+    let counter = 1;
 
     // extract the states here
     for (let rep in nigeriaReports) {
@@ -66,148 +66,197 @@ export default class Nigeria extends Component {
       );
     } else {
       return (
-        <main>
-            <h1 className="text-center">Nigeria Report</h1>
-            <Container>
+        <Container className="nigeria">
+            <h2 className="text-center">Nigeria Report</h2>
+            
               <Row>
                 {/* summary */}
-                <Col xs={12} sm={6} md={4} lg={3}>
-                  <Card className="text-center" bg="Light" variant="Light">
-                    <Card.Header>
-                      <h6>Summary</h6>
-                    </Card.Header>
-                    <Card.Body>
-                      <ListGroup variant="flush">
-                        {/* total confirmed */}
-                        <ListGroup.Item>
-                          Total Confirmed:{" "}
-                          <NumberFormat
-                            value={totalNigeriaReport.totalConfirmedCases}
-                            displayType={"text"}
-                            thousandSeparator={true}
-                            prefix={""}
-                          />
-                        </ListGroup.Item>
-
-                        {/* total deaths */}
-                        <ListGroup.Item>
-                          Total Deaths:{" "}
-                          <NumberFormat
-                            value={totalNigeriaReport.death}
-                            displayType={"text"}
-                            thousandSeparator={true}
-                            prefix={""}
-                          />
-                        </ListGroup.Item>
-
-                        {/* total discharged */}
-                        <ListGroup.Item>
-                          Total Discharged:{" "}
-                          <NumberFormat
-                            value={totalNigeriaReport.discharged}
-                            displayType={"text"}
-                            thousandSeparator={true}
-                            prefix={""}
-                          />
-                        </ListGroup.Item>
-
-                        {/* active cases */}
-                        <ListGroup.Item>
-                          Active Cases:{" "}
+                <Col xs={12} sm={12} md={4} lg={4}>
+                  <table id="each-countries">
+                    <tr>
+                      <th>
+                        000
+                        <span>
+                        Summary
+                        </span>
+                      </th>
+                    </tr>
+                    
+                    {/* active cases */}
+                    <tr>
+                      <td>
+                        Active Cases
+                        <br />
+                        <span style={{ color: "#FF9900" }}>
                           <NumberFormat
                             value={totalActive}
                             displayType={"text"}
                             thousandSeparator={true}
                             prefix={""}
                           />
-                        </ListGroup.Item>
+                        </span>
+                      </td>
+                    </tr>
 
-                        {/* total tested */}
-                        <ListGroup.Item>
-                          Total Tested:{" "}
+                    {/* recovered */}
+                    <tr>
+                      <td>
+                        Total Recovered
+                        <br />
+                        <span style={{ color: "#27AA25" }}>
+                          <NumberFormat
+                            value={totalNigeriaReport.discharged}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
+                    
+                    {/* total comfirmed */}
+                    <tr>
+                      <td>
+                        Total Comfirmed
+                        <br />
+                        <span style={{ color: "#0BC3D5" }}>
+                          <NumberFormat
+                            value={totalNigeriaReport.totalConfirmedCases}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
+
+                    {/* total deaths */}
+                    <tr>
+                      <td>
+                        Total Deaths
+                        <br />
+                        <span style={{ color: "#BB2626" }}>
+                          <NumberFormat
+                            value={totalNigeriaReport.death}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
+
+                    {/* total tested */}
+                    <tr>
+                      <td>
+                      Total Tested
+                        <br />
+                        <span style={{ color: "#0BC3D5" }}>
                           <NumberFormat
                             value={totalNigeriaReport.totalSamplesTested}
                             displayType={"text"}
                             thousandSeparator={true}
                             prefix={""}
                           />
-                        </ListGroup.Item>
-                      </ListGroup>
-                    </Card.Body>
-
-                    {/* last updated */}
-                    <Card.Footer className="text-muted">
-                      <p className="text-danger">Stay Safe and Updated</p>
-                    </Card.Footer>
-                  </Card>
+                        </span>
+                      </td>
+                    </tr>
+                    
+                  </table>
                 </Col>
 
                 {/* individual states */}
                 {stateReports.map((report) => (
-                  <Col xs={12} sm={6} md={4} lg={3}>
-                    <Card className="text-center" bg="Light" variant="Light">
-                      <Card.Header>
-                        <h6>{report.state}</h6>
-                      </Card.Header>
-                      <Card.Body>
-                        <ListGroup variant="flush">
-                          {/* total confirmed */}
-                          <ListGroup.Item>
-                            Total Confirmed:{" "}
-                            <NumberFormat
-                              value={report.confirmedCases}
-                              displayType={"text"}
-                              thousandSeparator={true}
-                              prefix={""}
-                            />
-                          </ListGroup.Item>
+                  <Col xs={12} sm={12} md={4} lg={4}>
+                  <table id="each-countries">
+                    <tr>
+                      <th>
+                        00{counter++}
+                        <span>
+                        {report.state}
+                        </span>
+                      </th>
+                    </tr>
+                    
+                    {/* active cases */}
+                    <tr>
+                      <td>
+                        Active Cases
+                        <br />
+                        <span style={{ color: "#FF9900" }}>
+                          <NumberFormat
+                            value={report.casesOnAdmission}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
 
-                          {/* total deaths */}
-                          <ListGroup.Item>
-                            Total Deaths:{" "}
-                            <NumberFormat
-                              value={report.death}
-                              displayType={"text"}
-                              thousandSeparator={true}
-                              prefix={""}
-                            />
-                          </ListGroup.Item>
+                    {/* recovered */}
+                    <tr>
+                      <td>
+                        Total Recovered
+                        <br />
+                        <span style={{ color: "#27AA25" }}>
+                          <NumberFormat
+                            value={report.discharged}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
+                    
+                    {/* total comfirmed */}
+                    <tr>
+                      <td>
+                        Total Comfirmed
+                        <br />
+                        <span style={{ color: "#0BC3D5" }}>
+                          <NumberFormat
+                            value={report.confirmedCases}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
 
-                          {/* total discharged */}
-                          <ListGroup.Item>
-                            Total Discharged:{" "}
-                            <NumberFormat
-                              value={report.discharged}
-                              displayType={"text"}
-                              thousandSeparator={true}
-                              prefix={""}
-                            />
-                          </ListGroup.Item>
+                    {/* total deaths */}
+                    <tr>
+                      <td>
+                        Total Deaths
+                        <br />
+                        <span style={{ color: "#BB2626" }}>
+                          <NumberFormat
+                            value={report.death}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={""}
+                          />
+                        </span>
+                      </td>
+                    </tr>
 
-                          {/* active cases */}
-                          <ListGroup.Item>
-                            Active Cases:{" "}
-                            <NumberFormat
-                              value={report.casesOnAdmission}
-                              displayType={"text"}
-                              thousandSeparator={true}
-                              prefix={""}
-                            />
-                          </ListGroup.Item>
-                        </ListGroup>
-                      </Card.Body>
-
-                      {/* last updated */}
-                      <Card.Footer className="text-muted">
-                        <p className="text-danger">Stay Safe and Updated</p>
-                      </Card.Footer>
-                    </Card>
-                  </Col>
+                    
+                    {/* Stay Safe Message */}
+                    <tr>
+                      <td style={{ color: "#BB2626", paddingTop: "4%" }}>
+                        <p>Stay Safe and Updated</p>
+                      </td>
+                    </tr>
+                    
+                  </table>
+                </Col>
                 ))}
               </Row>
-            </Container>
+            
             <Footer />
-        </main>
+            </Container>
       );
     }
   }
