ce00c6cb2ecca3d37e02b80492d1f2cd2a94b336
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-10 01:49:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     this.state = {
       country: "country",
       isLoaded: false,
-      reports: []
+      reports: [],
     };
 
     capitalize(this.state.country);
@@ -26,22 +26,22 @@ export default class App extends Component {
 
   handleSearchData(stateData) {
     this.setState({
-      ...stateData
+      ...stateData,
     });
   }
 
   componentDidMount() {
     fetch("https://api.coronatracker.com/v3/stats/worldometer/country")
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         this.setState({
           reports: json,
-          isLoaded: true
+          isLoaded: true,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({
-          error
+          error,
         });
       });
   }
@@ -59,118 +59,117 @@ export default class App extends Component {
         </div>
       );
     } else {
+      return (
+        <div className="app">
+          <NavComponent handleSearch={this.handleSearchData.bind(this)} />
+          {reports.map((report) =>
+            report.country === coutrySanitized.toUpperCase() ||
+            report.country === coutrySanitized.toLowerCase() ||
+            report.country === capitalize(coutrySanitized) ? (
+              <Col xs={12} sm={6} md={4} lg={3}>
+                <Card className="text-center" bg="Light" variant="Light">
+                  <Card.Header>
+                    <h6>
+                      {report.country} ({report.countryCode})
+                    </h6>
+                  </Card.Header>
+                  <Card.Body>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        Total Confirmed:{" "}
+                        <NumberFormat
+                          value={report.totalConfirmed}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Total Deaths:{" "}
+                        <NumberFormat
+                          value={report.totalDeaths}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Total Recovered:{" "}
+                        <NumberFormat
+                          value={report.totalRecovered}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Today's Confirmed:{" "}
+                        <NumberFormat
+                          value={report.dailyConfirmed}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Today's Deaths:{" "}
+                        <NumberFormat
+                          value={report.dailyDeaths}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Active Cases:{" "}
+                        <NumberFormat
+                          value={report.activeCases}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Total Critical:{" "}
+                        <NumberFormat
+                          value={report.totalCritical}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Total Confirmed Per Million Population:
+                        <br />
+                        <NumberFormat
+                          value={report.totalConfirmedPerMillionPopulation}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={""}
+                        />
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">
+                    Last Updated:
+                    <br />
+                    <Moment>{report.lastUpdated}</Moment>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ) : (
+              ""
+            )
+          )}
 
-    return (
-      <div className="app">
-        <NavComponent handleSearch={this.handleSearchData.bind(this)} />
-        {reports.map(report =>
-          report.country === coutrySanitized.toUpperCase() ||
-          report.country === coutrySanitized.toLowerCase() ||
-          report.country === capitalize(coutrySanitized) ? (
-            <Col xs={12} sm={6} md={4} lg={3}>
-              <Card className="text-center" bg="Light" variant="Light">
-                <Card.Header>
-                  <h6>
-                    {report.country} ({report.countryCode})
-                  </h6>
-                </Card.Header>
-                <Card.Body>
-                  <ListGroup variant="flush">
-                    <ListGroup.Item>
-                      Total Confirmed:{" "}
-                      <NumberFormat
-                        value={report.totalConfirmed}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Total Deaths:{" "}
-                      <NumberFormat
-                        value={report.totalDeaths}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Total Recovered:{" "}
-                      <NumberFormat
-                        value={report.totalRecovered}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Today's Confirmed:{" "}
-                      <NumberFormat
-                        value={report.dailyConfirmed}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Today's Deaths:{" "}
-                      <NumberFormat
-                        value={report.dailyDeaths}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Active Cases:{" "}
-                      <NumberFormat
-                        value={report.activeCases}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Total Critical:{" "}
-                      <NumberFormat
-                        value={report.totalCritical}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Total Confirmed Per Million Population:
-                      <br />
-                      <NumberFormat
-                        value={report.totalConfirmedPerMillionPopulation}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={""}
-                      />
-                    </ListGroup.Item>
-                  </ListGroup>
-                </Card.Body>
-                <Card.Footer className="text-muted">
-                  Last Updated:
-                  <br />
-                  <Moment>{report.lastUpdated}</Moment>
-                </Card.Footer>
-              </Card>
-            </Col>
-          ) : (
-            ""
-          )
-        )}
-
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/nigeria" component={Nigeria} />
-          <Route path="/news" component={News} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </div>
-    );
-          }
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/nigeria" component={Nigeria} />
+            <Route path="/news" component={News} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      );
+    }
   }
 }
